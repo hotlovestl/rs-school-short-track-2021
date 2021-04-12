5573149eb30a9e96c7aa829be6046bfd6f57e671
hotlovestl <pluhin34@gmail.com>
2021-04-12 23:51:15 +0300

    feat: add solution for 15-get-email-domain.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,7 +21,14 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
+function minesweeper(matrix) {
+  const pairs = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) pairs.push([i, j]);
+    }
+  }
+  return pairs;
 }
 
 module.exports = minesweeper;
--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const splitByAtCommercial = email.split('@');
+  return splitByAtCommercial[splitByAtCommercial.length - 1];
 }
 
 module.exports = getEmailDomain;
